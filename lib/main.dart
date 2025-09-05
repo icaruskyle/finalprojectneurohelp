d793b2e10586590dd91284bb667be6aae0810b94
PJCuevas31 <166819238+PJCuevas31@users.noreply.github.com>
2025-09-05 11:10:03 +0800

    09/05/2025 + updated dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'screens/welcome_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
-import 'screens/dashboard_screen.dart';
 
 void main() {
   runApp(const MyApp());
